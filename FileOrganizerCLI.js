27baf15acf7bd4cdd664c23c473c2a16ddbc5fbb
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2025-09-18 14:41:42 +0300

    Initial commit

--- FileOrganizerCLI.js
+++ FileOrganizerCLI.js
@@ -1,2 +1,7 @@
-console.log("File Organizer CLI");
-// code comming soon...
+/*
+** @Author: HamidReza Ghavami
+** @Date: 2025-09-18
+** @Description: A simple CLI tool to organize files into folders based on their extensions.
+*/
+
+// code will come here soon...
